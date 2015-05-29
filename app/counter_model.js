99f0d4007faec9378782d8d16c8086ec69099b2d
Thomas Logan <thomaslogan@Thomass-MacBook-Pro.local>
2015-05-28 17:58:54 -0700

    [feature] added chrome storage functionality.

--- app/counter_model.js
+++ app/counter_model.js
@@ -1,7 +1,23 @@
 //Creating the counter model
-var Counter = Backbone.Model.extend({});
+var Counter = Backbone.Model.extend({
+  //Attaching an event listener onto the counter model
+  initialize: function() {
+    this.on('change', function(){
+      chrome.storage.sync.set({'interviewCount': this.interviewCount}, function(){
+        console.log('Saved');
+      });
+    });
+    //Get Data from storage
+    var that = this;
+    chrome.storage.sync.get('interviewCount', function(count){
+      that.set({interviewCount: count.interviewCount});
+    });
+  }
+});
+
 
 //Creating a new instance of the counter model
 var counter = new Counter({
     interviewCount: 0
 });
+
--- app/counter_view.js
+++ app/counter_view.js
@@ -1,30 +1,21 @@
 //Creating a new view for the counter
 var CounterView = Backbone.View.extend({
-    id: 'viewCounter',
-    initialize: function() {
-      this.listenTo(this.model, "change", this.render);
-    },
-    render: function(){
-      var html = '<h1>' + this.model.get('interviewCount') + '</h1>';  
-      return this.$el.html(html);
-    },
-    events: {
-      'click h1': 'addInterview',
-      'click h1': 'saveChanges'
-    },
-    addInterview: function(){
-      var addOne = this.model.get('interviewCount');
-      addOne++;
-      this.model.set({interviewCount: addOne});
-      this.model.save({interviewCount: addOne});
-    },
-    saveChanges: function(){
-      var savedInterviews = this.model.get('interviewCount');
-      chrome.storage.sync.set({'interviewCount': savedInterviews}, function() {
-        //Notification that the data has been saved.
-        message('Saved Interviews');
-      });
-    }
+  id: 'viewCounter',
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+  },
+  render: function(){
+    var html = '<h1>' + this.model.get('interviewCount') + '</h1>';  
+    return this.$el.html(html);
+  },
+  events: {
+    'click h1': 'addInterview'
+  },
+  addInterview: function(){
+    var addOne = this.model.get('interviewCount');
+    addOne++;
+    this.model.set({interviewCount: addOne});
+  }
 });
 
 //Instantiating the view of the counter and referencing the counter model.
